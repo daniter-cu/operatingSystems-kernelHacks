026d2cf46914af4200211fff711fec77eada78b9
student <student@w4118.columbia.edu>
2010-03-26 16:23:48 -0400

    Dan is stupid

--- linux-2.6.11.12-hmwk4/kernel/sched.c
+++ linux-2.6.11.12-hmwk4/kernel/sched.c
@@ -648,7 +648,13 @@ static void dequeue_task(struct task_struct *p, prio_array_t *array)
 static void enqueue_task(struct task_struct *p, prio_array_t *array)
 {
 	sched_info_queued(p);
-	list_add_tail(&p->run_list, array->queue + p->prio);
+	if (p->prio == 100)
+	{
+	    //not right
+	    list_add_tail(&p->run_list, array->queue + p->prio);
+	}
+	else
+	    list_add_tail(&p->run_list, array->queue + p->prio);
 	__set_bit(p->prio, array->bitmap);
 	array->nr_active++;
 	p->array = array;
