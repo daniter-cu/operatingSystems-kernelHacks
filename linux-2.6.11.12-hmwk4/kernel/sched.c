d213ec11630cadca036956b3ae1e759cde1e8adc
student <student@w4118.columbia.edu>
2010-03-25 23:58:20 -0400

    fix colorProb matrix syntax

--- linux-2.6.11.12-hmwk4/kernel/sched.c
+++ linux-2.6.11.12-hmwk4/kernel/sched.c
@@ -38,7 +38,7 @@
 #include <linux/smp.h>
 #include <linux/threads.h>
 #include <linux/timer.h>
-#include <linux/rcupdate.>
+#include <linux/rcupdate.h>
 #include <linux/cpu.h>
 #include <linux/percpu.h>
 #include <linux/kthread.h>
@@ -51,10 +51,11 @@
 
 /* OS HW4 */
 /* Stores probabilities of color pairs.  Defaults to 0. */
-int[5][5] colorProbs = { {0,0,0,0,0},
+int colorProbs[5][5] =  { {0,0,0,0,0},
 		         {0,0,0,0,0},
 		         {0,0,0,0,0}, 
-		         {0,0,0,0,0} };
+		         {0,0,0,0,0},
+                         {0,0,0,0,0} };
 
 /*
  * Convert user-nice values [ -20 ... 0 ... 19 ]
